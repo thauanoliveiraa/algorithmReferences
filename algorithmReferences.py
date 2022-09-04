dd8dbf866424789759c1132198bb5db189a948b6
Thauan <thauanoliveiracontact2503@gmail.com>
2022-09-04 02:26:42 -0300

    Muitas coisas modificadas

--- algorithmReferences.py
+++ algorithmReferences.py
@@ -22,7 +22,6 @@ referenceData = tipo de referencia (livro, ...)
 """
 import os
 from time import sleep
-from weakref import ref
 clear = lambda: os.system('cls')
 referenceData = True
 
@@ -33,7 +32,27 @@ def smallTime():
     sleep(2)
 
 def biggestTime():
-    sleep(4)    
+    sleep(4)
+
+
+#VERIFICAÇÃO QUANDO USAR INTEIRO
+def verify_use_integer(variable, verification):
+    if variable.isnumeric():
+        variable = (int(variable)) 
+    elif variable.isalpha() or variable.isalnum() or variable == 0:
+        clear()
+        print("Erro, tente novamente")
+        sleep(2)
+        verification = True
+
+          
+#VERIFICAÇÃO QUANDO USAR STRING
+def verify_use_string(variable, verification):  
+    if variable.isnumeric() or variable.isdigit() or variable == '':
+        clear()
+        print("Erro, tente novamente")
+        sleep(2)
+        verification = True
 
 def poweredBy():
     author = "-- REFERENCE FORMAT AND CREATOR SCRIPT -- \nPowered by Thauan Oliveira ©\nInstagram : @thx0liver \nGithub : https://github.com/thauanoliveiraa\nLinkedin : https://www.linkedin.com/in/thauan-de-oliveira-ramos-396b66224/\n"
@@ -41,41 +60,61 @@ def poweredBy():
 
 term_of_use = True
 while term_of_use:
+    clear()
     poweredBy()
     term_of_use = input("\nDo you agree to use and agree to all terms imposed by the author ?\n\n Say 'Y' (Yes) or 'N' (No) : ").upper()
     if term_of_use.isalpha() and term_of_use == 'Y':
         while referenceData:
             clear()
             smallTime()
-            referenceData = input("-- TIPOS DE REFERENCIA --\n1 - LIVRO\n2 - ARTIGO ONLINE\n3 - ARTIGO DE REVISTA\n4 - CONSTITUIÇÃO FEDERAL OU ESTADUAL\n5 - LEGISLAÇÃO COMUM\n6 - TESE\n7 - SAIR\nPowered by : \nInsira o tipo de referencia a ser criada : ")
+            referenceData = input("-- TIPOS DE REFERENCIA --\n1 - LIVRO\n2 - ARTIGO ONLINE\n3 - ARTIGO DE REVISTA\n4 - CONSTITUIÇÃO FEDERAL OU ESTADUAL\n5 - LEGISLAÇÃO COMUM\n6 - TESE\n7 - SAIR\n\nInsira o tipo de referencia a ser criada : ")
             if referenceData.isnumeric():
                 referenceData = (int(referenceData)) 
                 if referenceData == 1:
                     clear()
                     print("REFERÊNCIA PARA LIVRO")
                     authorQuantity = True
-                    while authorQuantity:
-                        authorQuantity = input("Insira a quantidade de autores : ")
-                        if authorQuantity.isnumeric():
-                            authorQuantity = (int(authorQuantity))
-                            if authorQuantity == 0:
-                                #== 0 - INSERIR : Título, Cidade, Editora, Ano de publicação, Página
-                                Title = print("TESTE")
-                                city = 
-                                publishingCompany = 
-                                publishingYear = 
-                                page = 
-                            elif authorQuantity == 1:
-                                print("TESTE")    
-                            elif authorQuantity <= 3:
-                                print("TESTE")  
-                            elif authorQuantity > 3:
-                                print("TESTE")                                                       
-                        else:
-                            clear()
-                            print("O que foi digitado não é um número, tente novamente")
-                            smallTime()
-                            authorQuantity = True                   
+                    #while authorQuantity:
+                    authorQuantity = input("Insira a quantidade de autores : ")
+                    if authorQuantity.isnumeric():
+                        authorQuantity = (int(authorQuantity))
+                            #for i in range(authorQuantity):
+                            #for i in range(authorQuantity):
+                                #verification = True
+                        if authorQuantity == 0:
+                            verification = True
+                            while verification:
+                                clear()
+                                title = input("Digite o título : ")
+                                verify_use_string(title, verification=True)
+
+                                city = input("Digite a a cidade : ")
+                                verify_use_string(city, verification=True) 
+
+                                publishingCompany = input("Digite a editora : ")
+                                verify_use_string(publishingCompany, verification=True)
+
+                                publishingYear = input("Digite o ano de publicação : ")
+                                verify_use_integer(publishingYear, verification=True)
+
+                                page = input("Digite a página : ")
+                                verify_use_integer(page, verification=True)
+                                # CRIAR LOGICA DE FORMATAÇÃO DE STRING
+                                #bookReferenceNotAuthor = 
+                                verification=False
+
+                                elif authorQuantity == 1:
+                                    print("TESTE")    
+                                elif authorQuantity <= 3:
+                                    print("TESTE")  
+                                elif authorQuantity > 3:
+                                    print("TESTE")                                                 
+                            
+                    #else:
+                     #   clear()
+                      #  print("O que foi digitado não é um número, tente novamente")
+                       # smallTime()
+                        #authorQuantity = True                   
                     biggestTime()
                 elif referenceData == 2:
                     clear()
